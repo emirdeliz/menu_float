149b80487e03315d34eb21e17804ac2956f6de1f
emirdeliz <emir.marques@condoconta.com>
2022-08-23 09:56:30 -0300

    feat: Work in progress

--- exec_test.sh
+++ exec_test.sh
@@ -3,7 +3,10 @@
 #   --target=integration_test/menu_float_test.dart \
 #   -d chrome
 
-flutter drive \
-  --driver=driver_test/integration_test.dart \
-  --target=integration_test/menu_float_test.dart \
-  -d web-server --browser-name=chrome --profile
+# flutter drive \
+#   --driver=driver_test/integration_test.dart \
+#   --target=integration_test/menu_float_test.dart \
+#   -d chrome --browser-name=chrome --profile
+
+flutter test \
+	&& fswatch -e ".*" -i "\\.dart$" . | xargs -n1 -I {} sh -c "echo $'\e[1;31m'{}$'\e[0m'; flutter test -j 2 ./test"
--- test/menu_float_test.dart
+++ test/menu_float_test.dart
@@ -1,10 +1,39 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'menu_float_app_test.dart';
+
+const String TITLE_WIDGET_TEST = 'Click me';
+const double PORTRAIT_WIDTH = 1024;
+const double PORTRAIT_HEIGHT = 767;
+const double LANDSCAPE_WIDTH = PORTRAIT_HEIGHT;
+const double LANDSCAPE_HEIGHT = PORTRAIT_WIDTH;
+
+Widget makeTargetButton() {
+  return ElevatedButton(
+      onPressed: () => {}, child: const Text(TITLE_WIDGET_TEST));
+}
+
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  final TestWidgetsFlutterBinding binding =
+      TestWidgetsFlutterBinding.ensureInitialized();
+  group('have menu for the button', () {
+    testWidgets('finds a Text widget', (tester) async {
+      await binding.setSurfaceSize(const Size(PORTRAIT_WIDTH, PORTRAIT_HEIGHT));
+
+      final target = makeTargetButton();
+      await tester.pumpWidget(MenuFloatAppTest(target: target, x: 0, y: 0));
+
+      await tester.pumpAndSettle();
+      final button = find.byWidget(target);
+
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+
+      final menu = find.text(TITLE_WIDGET_TEST);
+      expect(menu, findsOneWidget);
+    });
   });
 }
